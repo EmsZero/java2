e57d0edf49bf8cb541ca775ccecf0e7c4d76aa23
Sergio <sergio.hinojosa.avila@gmail.com>
2015-09-07 12:11:40 -0400

    ejercicio 1

---
+++ prueba.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.io.util.*;
+
+public class Odds{
+
+    public static void main(String [] args){
+        int juan=0, maria = 0, x, n, i;
+        Scanner in = new Scanner(System.in);
+        while ((n= in.nextInt())> 0){
+        }
+    }
+
+}
