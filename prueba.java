1074a16a3200bdc6ee5ae1482418628e1ac443df
Ems <zeroalphapro@gmail.com>
2015-09-07 11:23:05 -0500

    Update prueba.java

--- prueba.java
+++ prueba.java
@@ -7,7 +7,18 @@ public class Odds{
         int juan=0, maria = 0, x, n, i;
         Scanner in = new Scanner(System.in);
         while ((n= in.nextInt())> 0){
+            Juan = 0;
+            Maria = 0;
+            for (i = 0; i < n; i++) {
+                x= in.nextInt();
+                if ((x & 1) == 0)
+                Juan++;
+            }
         }
+        if ( Juan > Maria )
+            System.out.printIn(Juan - Maria) ;
+        else
+            System.out.printIn(Maria - Juan) ;
     }
 
 }
